114ae833c49c34ca76341233f5c9752d84b0126d
SattikBhatt <84816480+SattikBhatt@users.noreply.github.com>
2023-10-26 16:51:03 +0530

    Update Meme.jsx

--- RiddleGenerator/src/Meme.jsx
+++ RiddleGenerator/src/Meme.jsx
@@ -1,8 +1,8 @@
  import React from 'react'
  import ReactDOM from 'react-dom'
 import App from './App'
-import '../Meme.css'
+import '../riddles.css'
 // const App=require('./memegenerator/src/App')
-ReactDOM.createRoot(document.getElementById('root')).render(<div className="mememain">
+ReactDOM.createRoot(document.getElementById('root')).render(<div className="riddlemain">
 <App/>
 </div>,document.getElementById('root'))
